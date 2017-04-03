7ff9aeff149ef5e112ce5e2b2e498c0aa7e74367
David Shao <david.zeratul53@gmail.com>
2017-04-03 15:07:17 -0700

    first

---
+++ app/src/main/java/com/fomono/fomono/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fomono.fomono.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fomono.fomono.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
