4f1b72fea74579dbba9851e576e483fb5aa16fc5
Malle Saranu <malle.saranu@gmail.com>
2017-04-15 22:52:08 -0700

    Sign-in as Guest

--- app/src/main/java/com/fomono/fomono/fragments/FomonoDetailMoviedbFragment.java
+++ app/src/main/java/com/fomono/fomono/fragments/FomonoDetailMoviedbFragment.java
@@ -17,6 +17,7 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
