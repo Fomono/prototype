c0f4b36d8e7623d3da167e4b3225cd00c290fc4c
David Shao <david.zeratul53@gmail.com>
2017-04-16 23:08:29 -0700

    dont log in again if user is already logged in, populate user image/name, various fixes

--- app/src/main/java/com/fomono/fomono/supportclasses/NavigationDrawerClass.java
+++ app/src/main/java/com/fomono/fomono/supportclasses/NavigationDrawerClass.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.fomono.fomono.R;
 import com.fomono.fomono.activities.FomonoFilterActivity;
-import com.fomono.fomono.activities.UserProfileActivity;
+import com.fomono.fomono.activities.LoginUserActivity;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
 
 /**
  * Created by jsaluja on 4/14/2017.
@@ -26,6 +27,10 @@ public class NavigationDrawerClass {
     }
 
     public void setupDrawerContent(NavigationView navigationView) {
+        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+            MenuItem item = navigationView.getMenu().findItem(R.id.drawerSignOutId);
+            item.setTitle(R.string.drawer_menu_signin);
+        }
         navigationView.setNavigationItemSelectedListener(
                 menuItem -> {
                     selectDrawerItem(menuItem);
@@ -38,15 +43,19 @@ public class NavigationDrawerClass {
 
         switch (menuItem.getItemId()) {
             case R.id.drawerAboutId:
-                Intent aboutIntent = new Intent(mContext,FomonoFilterActivity.class);
-                mContext.startActivity(aboutIntent);
+                //TODO: implement about screen
+                Toast.makeText(mContext, "This app is awesome.", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.drawerFilterId:
                 Intent filterIntent = new Intent(mContext,FomonoFilterActivity.class);
                 mContext.startActivity(filterIntent);
                 break;
             case R.id.drawerSignOutId:
-                Toast.makeText(mContext, "Go to sign out activity!!", Toast.LENGTH_SHORT).show();
+                if (!ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+                    ParseUser.logOutInBackground();
+                }
+                Intent i = new Intent(mContext, LoginUserActivity.class);
+                mContext.startActivity(i);
             default:
                 break;
         }
