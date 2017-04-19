bdf61bd25e0a9b59dc32766c9970c7df746263d3
jsaluja87 <saluja.jaspreetsingh@gmail.com>
2017-04-19 00:01:32 -0700

    god damn list item UI

--- app/src/main/java/com/fomono/fomono/adapters/ViewHolderEventsItem.java
+++ app/src/main/java/com/fomono/fomono/adapters/ViewHolderEventsItem.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fomono.fomono.R;
@@ -20,9 +21,8 @@ public class ViewHolderEventsItem extends RecyclerView.ViewHolder {
 
     public TextView eventName;
     public TextView eventDistance;
-    public com.makeramen.roundedimageview.RoundedImageView eventMediaImage;
+    public ImageView eventMediaImage;
     public TextView eventPrice;
-    public TextView eventDesc;
     public TextView eventDateTime;
     public TextView eventType;
     public ImageButton eventUrl;
@@ -34,7 +34,6 @@ public class ViewHolderEventsItem extends RecyclerView.ViewHolder {
         eventDistance = binding.EventDistanceId;
         eventMediaImage = binding.EventMediaImageId;
         eventPrice = binding.EventPriceId;
-        eventDesc = binding.EventDescId;
         eventDateTime = binding.EventDateTimeId;
         eventType = binding.EventTypeId;
         eventUrl = binding.ImageLogoButtonId;
